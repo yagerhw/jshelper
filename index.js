ed06674750e96f5ac791b4ac487d170c41e83e50
Yager Guo <yagerguo@126.com>
2022-03-22 11:39:04 +0800

    1.0.0

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+export class Returnhw {
+
+    constructor(code, message = '', data = null) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+}
